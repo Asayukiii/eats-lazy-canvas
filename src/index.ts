6550df2a46ebcd51c1192bfe221973fb2652cbac
Asayukiii <alphadlpop6@gmail.com>
2026-05-19 11:50:10 -0600

    feat: initialize EATSLazyCanvas addon with lazy-canvas integration

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Addon, APIFunction, type API } from 'easy-api.ts'
 import { join } from 'path'
 import { inspect } from 'util'
 import { Logger, LogPriority } from './classes/Logger'
+import type { LazyCanvasPlugin } from '@hitomihiumi/lazy-canvas'
 
 /**
  * Directory where the functions are located.
@@ -25,10 +26,32 @@ export const packageName = '@eats/lazy-canvas'
  * Logger instance.
  * @private
  */
-const logger = new Logger({
-    level: LogPriority.DEBUG,
-    prefix: '@eats/lazy-canvas'
-})
+export let logger: Logger
+
+/**
+ * Options for the EATSLazyCanvas class.
+ */
+export interface EATSLazyCanvasOptions {
+    /**
+     * Debug mode.
+     * @default false
+     */
+    debug?: boolean
+    /**
+     * Plugins for the canvas.
+     * @default []
+     */
+    plugins?: LazyCanvasPlugin[]
+}
+
+/**
+ * Default options for the EATSLazyCanvas class.
+ * @private
+ */
+const defaultOptions: Required<EATSLazyCanvasOptions> = {
+    debug: false,
+    plugins: []
+}
 
 /**
  * kiss, kiss. <3
@@ -37,11 +60,20 @@ export class EATSLazyCanvas extends Addon {
     override name = packageName
     override description = 'lazy-canvas implementation for easy-api.ts.'
     override version = '1.0.0'
-    constructor(private readonly debug = false) {
+    private logger!: Logger
+    constructor(private readonly options: EATSLazyCanvasOptions = defaultOptions) {
         super()
+
+        this.logger = new Logger({
+            level: this.options.debug ? LogPriority.DEBUG : LogPriority.INFO,
+            prefix: packageName
+        })
+        logger = this.logger
     }
     override init(api: API): void {
+        this.logger.debug('Initializing EATSLazyCanvas...')
         api.functions.delete(NATIVE_CANVAS_PARENT_FUNCTION.toLowerCase())
         api.functions.load(FUNCTIONS_DIRECTORY)
+        this.logger.debug('Initialized EATSLazyCanvas successfully.')
     }
 }
